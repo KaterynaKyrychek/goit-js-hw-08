c582ef03b32cbe124297ae40cc3a6a578ce975da
KaterynaKyrychek <90039728+KaterynaKyrychek@users.noreply.github.com>
2022-04-27 16:19:45 +0300

    v3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const currentTime = 'videoplayer-current-time';
+const timeRestore = localStorage.getItem(currentTime);
+
+player.on(
+    'timeupdate',
+    throttle(data => {
+    localStorage.setItem(currentTime, Math.floor(data.seconds));
+    }, 1000),
+);
+
+if (timeRestore) {
+    player.setCurrentTime(timeRestore);
+}
