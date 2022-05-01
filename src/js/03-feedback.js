c8741366409308f8f363fa6be5aac10d8a71afa3
KaterynaKyrychek <90039728+KaterynaKyrychek@users.noreply.github.com>
2022-05-01 10:49:18 +0300

    v8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,49 +1,34 @@
-import throttle from "lodash.throttle";
-
-const formKey = "feedback-form-state";
-
+import throttle from 'lodash.throttle';
+const FORM_KEY = 'feedback-form-state';
+const formData = {};
 const form = document.querySelector('.feedback-form');
-
-form.addEventListener('submit', event => {
-    event.preventDefault();
-    const formData = new FormData(form);
-    formData.forEach((value, name) => console.log(value, name));
-    // {
-    //     formData[name] = value;
-    // });
-
-});
-
-form.addEventListener('reset', () => {
-    localStorage.removeItem('formKey')
-});
-
-form.addEventListener('change', event => {
-    let parsedFilters = localStorage.getItem('formKey');
-    parsedFilters = parsedFilters ? JSON.parse
-    (parsedFilters) : [ ];
-    parsedFilters[event.target.name] = event.target.value;
-
-    if (parsedFilters) {
-        localStorage.setItem('formKey', JSON.stringify(parsedFilters));
-    }
-});
-
-// form.addEventListener('input', throttle(onInput => {
-//     formData.email = form.elements.email.value;
-//     formData.message = form.elements.message.value;
-//     localStorage.setItem('formKey', JSON.stringify(formData));
-// }, 1000),
-// );
-
+fillTheForm();
 function fillTheForm() {
-    let parsedFilters = localStorage.getItem('formKey');
-    if (parsedFilters) {
-        const { email, message } = JSON.parse(parsedFilters);
-        form.email.value = email;
-        form.message.value = message;
-        formData.email = email;
-        formData.message = message;
+    const enteredData = localStorage.getItem(FORM_KEY);
+    if (enteredData) {
+    const { email, message } = JSON.parse(enteredData);
+    form.email.value = email;
+    form.message.value = message;
+    formData.email = email;
+    formData.message = message;
     }
-
 }
+form.addEventListener(
+    'input',
+    throttle(onInput => {
+    formData.email = form.elements.email.value;
+    formData.message = form.elements.message.value;
+    localStorage.setItem(FORM_KEY, JSON.stringify(formData));
+    }, 500),
+);
+form.addEventListener('submit', onSubmit => {
+    onSubmit.preventDefault();
+    const formDataToSend = new FormData(onSubmit.currentTarget);
+    formDataToSend.forEach((value, name) => {
+    formData[name] = value;
+    });
+    if (!formData.email || !formData.message) return;
+    onSubmit.currentTarget.reset();
+    localStorage.removeItem(FORM_KEY);
+    console.log(formData);
+});
